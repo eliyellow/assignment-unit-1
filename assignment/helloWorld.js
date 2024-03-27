673bf8e8a5d4dcbcd1849c71214adf81c39fd478
eliyellow <160084188+eliyellow@users.noreply.github.com>
2024-03-27 11:04:59 -0500

    Update helloWorld.js

---
+++ assignment/helloWorld.js
@@ -0,0 +1,64 @@
+console.log('Hello World!');
+
+// Variables and data types
+
+// String - represent text
+let name = 'Chris'; 
+// variable name    value
+let     x        =    1; 
+
+//Number - represent numbers
+let total = 19;
+total = total + 9;
+
+console.log('total', total);
+
+// Boolean - true or false
+let inStock = true; 
+inStock = false;
+
+// Conditionals if / else
+
+if (inStock = true) {
+  // This code only runs if inStock is true
+  total += 5;
+  console.log('Adding to total: ', total);
+} else { // else is optional 
+  // This will run if inStock is not true'
+  console.log('Out of stock!');
+}
+
+let message = '';
+if (total > 200) {
+  message = 'Max cart value.";
+  console.log('message');
+} else if(total<= 0) {
+  console.log('Your cart is empty!');
+} else {
+  console.log('Total within 0 - 200');
+}
+
+let a = true;
+// === checking if the values are the same
+// = cahnge the value of a variable
+
+if (a === false) {
+  console.log('a is false', a);
+}
+
+console.log(a is ', a);
+
+let temp = 70;
+let isRaining = false;
+
+// && requires both conditions to be true
+if (temp > 65 && isRaining === false) {
+  console.log('Go for a walk.');
+  { else {
+    console.log(Stay inside.');
+      } 
+
+   // ! flips from true to false
+   isRaining = !isRaining;
+
+
